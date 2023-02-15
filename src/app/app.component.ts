28a9cbed31a69e191f7b7a1fd6ca35fa85ea30cc
rushikesh khataki <rushi.khataki14@gmail.com>
2023-02-15 20:24:32 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'assignment-3';
+  quarters = ["Quarter 1", "Quarter 2", "Quarter 3", "Quarter  4"];
+
+  months = ["Apr", "May", "Jun"];
+
+  selectedquarter = "Quarter 1";
+
+  selectedmonth = "Apr";
+
+  quarterchanged(){
+    if(this.selectedquarter == "Quarter 1"){
+      this.months = ["Apr", "May", "Jun"];
+      this.selectedmonth ="Apr";
+    }
+    else if(this.selectedquarter == "Quarter 2"){
+      this.months = ["Jul", "Aug", "Sep"];
+      this.selectedmonth ="Jul";
+    }
+    else if(this.selectedquarter == "Quarter 3"){
+      this.months = ["Oct", "Nov", "Dec"];
+      this.selectedmonth ="Oct";
+    }
+    else{
+      this.months = ["Jan", "Feb", "Mar"];
+      this.selectedmonth ="Jan";
+    }
+}
 }
